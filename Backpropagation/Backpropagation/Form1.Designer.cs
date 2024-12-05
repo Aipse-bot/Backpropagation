e2e0ce4296ed0366c6166d6287772610b24f5588
Kent Espia <151008963+Aipse-bot@users.noreply.github.com>
2024-12-06 02:08:29 +0800

    Add files via upload

--- Backpropagation/Backpropagation/Form1.Designer.cs
+++ Backpropagation/Backpropagation/Form1.Designer.cs
@@ -36,11 +36,30 @@
             this.button3 = new System.Windows.Forms.Button();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(214, 104);
+            this.textBox1.Location = new System.Drawing.Point(142, 92);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(147, 20);
             this.textBox1.TabIndex = 0;
@@ -48,7 +67,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(214, 156);
+            this.textBox2.Location = new System.Drawing.Point(142, 144);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(147, 20);
             this.textBox2.TabIndex = 1;
@@ -56,7 +75,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(431, 144);
+            this.textBox3.Location = new System.Drawing.Point(526, 144);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(170, 20);
             this.textBox3.TabIndex = 2;
@@ -64,7 +83,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(214, 261);
+            this.button1.Location = new System.Drawing.Point(149, 264);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 44);
             this.button1.TabIndex = 3;
@@ -74,7 +93,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(338, 261);
+            this.button2.Location = new System.Drawing.Point(273, 264);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(136, 44);
             this.button2.TabIndex = 4;
@@ -84,7 +103,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(526, 261);
+            this.button3.Location = new System.Drawing.Point(461, 264);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 44);
             this.button3.TabIndex = 5;
@@ -94,7 +113,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(214, 130);
+            this.textBox4.Location = new System.Drawing.Point(142, 118);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(147, 20);
             this.textBox4.TabIndex = 6;
@@ -102,16 +121,193 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(214, 182);
+            this.textBox5.Location = new System.Drawing.Point(142, 170);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(147, 20);
             this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(225, 363);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 8;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(369, 363);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 9;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(357, 144);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 10;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(363, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Hidden Neurons";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 347);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Trains";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(366, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Efficiency";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(196, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Inputs";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(590, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Outputs";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(593, 337);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 32);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Minimum Train";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(34, 337);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 32);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Minimum Learn";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(593, 311);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 18;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(34, 311);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(593, 295);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Minimum Trains";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 295);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Minimum Learn";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 250);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(116, 13);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Hidden Neurons to test";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(34, 266);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 22;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(570, 250);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(166, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Maximum tries to find the minimum";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(593, 266);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 24;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button3);
@@ -122,6 +318,7 @@
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,6 +334,25 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox12;
     }
 }
 
--- Backpropagation/Backpropagation/Form1.cs
+++ Backpropagation/Backpropagation/Form1.cs
@@ -8,12 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Backprop;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Backpropagation
 {
     public partial class Form1 : Form
     {
         NeuralNet nn;
+        private int clickCount = 0;
+        private int hiddenNeurons = 0;
         public Form1()
         {
             InitializeComponent();
@@ -21,6 +24,38 @@ namespace Backpropagation
 
         private void button2_Click(object sender, EventArgs e)
         {
+
+            string textFromTextBox8 = textBox8.Text;
+            int INTvalue;
+            if(int.TryParse(textFromTextBox8, out INTvalue))
+            {
+                hiddenNeurons = INTvalue;
+            }
+            else
+            {
+                hiddenNeurons = 100;
+            }
+
+            string textFromTextBox3 = textBox3.Text;
+            float FLOATvalue;
+
+            if (float.TryParse(textBox3.Text, out FLOATvalue))
+            {
+                textBox7.Text = textFromTextBox3;
+
+                if (FLOATvalue <= 0.90)
+                {
+                    clickCount++;
+                }
+            }
+            else
+            {
+                textBox7.Text = textFromTextBox3;
+                clickCount++;
+            }
+
+            textBox6.Text = hiddenNeurons  + " * " + clickCount;
+
             double[,] inputs = {
                 { 0.0, 0.0, 0.0, 0.0 },
                 { 0.0, 0.0, 0.0, 1.0 },
@@ -45,7 +80,7 @@ namespace Backpropagation
                 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0
             };
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < hiddenNeurons; i++)
             {
                 for (int j = 0; j < 16; j++)
                 {
@@ -62,6 +97,7 @@ namespace Backpropagation
 
     private void button3_Click(object sender, EventArgs e)
         {
+            
             nn.setInputs(0, Convert.ToDouble(textBox1.Text));
             nn.setInputs(1, Convert.ToDouble(textBox4.Text));
             nn.setInputs(2, Convert.ToDouble(textBox2.Text));
@@ -70,6 +106,18 @@ namespace Backpropagation
             nn.run();
 
             textBox3.Text = "" + nn.getOuputData(0);
+
+            string textFromTextBox3 = textBox3.Text;
+            float value;
+
+            if (float.TryParse(textBox3.Text, out value))
+            {
+                textBox7.Text = textFromTextBox3;
+            }
+            else
+            {
+                textBox7.Text = "Not Float";      
+            }
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -84,7 +132,8 @@ namespace Backpropagation
 
         private void button1_Click(object sender, EventArgs e)
         {
-            nn = new NeuralNet(4, 100, 1);
+            nn = new NeuralNet(4, hiddenNeurons, 1);
+            clickCount = 0;
         }
 
         private void textBox4_TextChanged(object sender, EventArgs e)
@@ -93,6 +142,277 @@ namespace Backpropagation
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+        int max= 0;
+        
+        int.TryParse(textBox12.Text, out max);
+     
+
+                //1st step
+                nn = new NeuralNet(4, hiddenNeurons, 1);
+                clickCount = 0;
+
+                //2nd step
+
+                for(int t = 0; t < max; t++)
+                {
+                    string textFromTextBox8 = textBox8.Text;
+                    int INTvalue;
+                        if (int.TryParse(textFromTextBox8, out INTvalue))
+                        {
+                            hiddenNeurons = INTvalue;
+                        }
+                        else
+                        {
+                            hiddenNeurons = 100;
+                        }
+
+                        string textFromTextBox3 = textBox3.Text;
+                        float FLOATvalue;
+
+                        if (float.TryParse(textBox3.Text, out FLOATvalue))
+                        {
+                            textBox7.Text = textFromTextBox3;
+
+                            if (FLOATvalue <= 0.90)
+                            {
+                                clickCount++;
+                            }
+                        }
+                        else
+                        {
+                            textBox7.Text = textFromTextBox3;
+                            clickCount++;
+                        }
+
+                        textBox6.Text = hiddenNeurons + " * " + clickCount;
+                        textBox9.Text = hiddenNeurons + " * " + clickCount;
+
+                double[,] inputs = {
+                        { 0.0, 0.0, 0.0, 0.0 },
+                        { 0.0, 0.0, 0.0, 1.0 },
+                        { 0.0, 0.0, 1.0, 0.0 },
+                        { 0.0, 0.0, 1.0, 1.0 },
+                        { 0.0, 1.0, 0.0, 0.0 },
+                        { 0.0, 1.0, 0.0, 1.0 },
+                        { 0.0, 1.0, 1.0, 0.0 },
+                        { 0.0, 1.0, 1.0, 1.0 },
+                        { 1.0, 0.0, 0.0, 0.0 },
+                        { 1.0, 0.0, 0.0, 1.0 },
+                        { 1.0, 0.0, 1.0, 0.0 },
+                        { 1.0, 0.0, 1.0, 1.0 },
+                        { 1.0, 1.0, 0.0, 0.0 },
+                        { 1.0, 1.0, 0.0, 1.0 },
+                        { 1.0, 1.0, 1.0, 0.0 },
+                        { 1.0, 1.0, 1.0, 1.0 }
+                    };
+
+                        double[] outputs = {
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0
+                    };
+
+                        for (int i = 0; i < hiddenNeurons; i++)
+                        {
+                            for (int j = 0; j < 16; j++)
+                            {
+                                nn.setInputs(0, inputs[j, 0]);
+                                nn.setInputs(1, inputs[j, 1]);
+                                nn.setInputs(2, inputs[j, 2]);
+                                nn.setInputs(3, inputs[j, 3]);
+                                nn.setDesiredOutput(0, outputs[j]);
+                                nn.learn();
+                            }
+                        }
+                    nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+                    nn.setInputs(1, Convert.ToDouble(textBox4.Text));
+                    nn.setInputs(2, Convert.ToDouble(textBox2.Text));
+                    nn.setInputs(3, Convert.ToDouble(textBox5.Text));
+
+                    nn.run();
+
+                    textBox3.Text = "" + nn.getOuputData(0);
+
+                    textFromTextBox3 = textBox3.Text;
+                    float value;
+
+                    if (float.TryParse(textBox3.Text, out value))
+                    {
+                        textBox7.Text = textFromTextBox3;
+                    }
+                    else
+                    {
+                        textBox7.Text = "Not Float";
+                    }
+            }
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int max = 0;
+            float prev = 0;
+            int min = 0;
+            float FLOATvalue = 0.0f;
+
+            int.TryParse(textBox11.Text, out max);
+            int f;
+            for (f = 0; f < max; f++)
+            {
+                //1st step
+                nn = new NeuralNet(4, f, 1);
+                clickCount = 0;
+
+                //2nd step
+
+
+                    hiddenNeurons = f;
+
+
+                    
+
+                    double[,] inputs = {
+                        { 0.0, 0.0, 0.0, 0.0 },
+                        { 0.0, 0.0, 0.0, 1.0 },
+                        { 0.0, 0.0, 1.0, 0.0 },
+                        { 0.0, 0.0, 1.0, 1.0 },
+                        { 0.0, 1.0, 0.0, 0.0 },
+                        { 0.0, 1.0, 0.0, 1.0 },
+                        { 0.0, 1.0, 1.0, 0.0 },
+                        { 0.0, 1.0, 1.0, 1.0 },
+                        { 1.0, 0.0, 0.0, 0.0 },
+                        { 1.0, 0.0, 0.0, 1.0 },
+                        { 1.0, 0.0, 1.0, 0.0 },
+                        { 1.0, 0.0, 1.0, 1.0 },
+                        { 1.0, 1.0, 0.0, 0.0 },
+                        { 1.0, 1.0, 0.0, 1.0 },
+                        { 1.0, 1.0, 1.0, 0.0 },
+                        { 1.0, 1.0, 1.0, 1.0 }
+                    };
+
+                    double[] outputs = {
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0
+                    };
+
+                    for (int i = 0; i < hiddenNeurons; i++)
+                    {
+                        for (int j = 0; j < 16; j++)
+                        {
+                            nn.setInputs(0, inputs[j, 0]);
+                            nn.setInputs(1, inputs[j, 1]);
+                            nn.setInputs(2, inputs[j, 2]);
+                            nn.setInputs(3, inputs[j, 3]);
+                            nn.setDesiredOutput(0, outputs[j]);
+                            nn.learn();
+                        }
+                    }
+                    nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+                    nn.setInputs(1, Convert.ToDouble(textBox4.Text));
+                    nn.setInputs(2, Convert.ToDouble(textBox2.Text));
+                    nn.setInputs(3, Convert.ToDouble(textBox5.Text));
+
+                    nn.run();
+                    textBox3.Text = "" + nn.getOuputData(0);
+
+                    string textFromTextBox3 = textBox3.Text;
+                    float.TryParse(textBox3.Text, out FLOATvalue);
+
+                    prev = FLOATvalue;
+
+
+
+
+                    for (int i = 0; i < hiddenNeurons; i++)
+                    {
+                        for (int j = 0; j < 16; j++)
+                        {
+                            nn.setInputs(0, inputs[j, 0]);
+                            nn.setInputs(1, inputs[j, 1]);
+                            nn.setInputs(2, inputs[j, 2]);
+                            nn.setInputs(3, inputs[j, 3]);
+                            nn.setDesiredOutput(0, outputs[j]);
+                            nn.learn();
+                        }
+                    }
+                    nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+                    nn.setInputs(1, Convert.ToDouble(textBox4.Text));
+                    nn.setInputs(2, Convert.ToDouble(textBox2.Text));
+                    nn.setInputs(3, Convert.ToDouble(textBox5.Text));
+
+                    nn.run();
+
+                textBox3.Text = "" + nn.getOuputData(0);
+
+                textFromTextBox3 = textBox3.Text;
+                float.TryParse(textBox3.Text, out FLOATvalue);
+
+                
+
+                textFromTextBox3 = textBox3.Text;
+
+                if (prev < FLOATvalue)
+                {
+                    textBox10.Text = "Learn at " + f;
+                    break;
+                }
+                else
+                {
+                    textBox10.Text = "dumb";
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
         {
 
         }
